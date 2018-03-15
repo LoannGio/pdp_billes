44d5db5cce653ddb84fb4fabf4d153fbe807c8a0
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-03-15 16:46:44 +0100

    test git push

--- pdp_billes/src/_defaut/Main.java
+++ pdp_billes/src/_defaut/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Initialiser l'instance du controller qui instancie le model
 		MainFrame window = new MainFrame();
-
+		String testGIT = "useless modification to test git push on both gits repositories"; // TODO
+																							// delete
 	}
 }
