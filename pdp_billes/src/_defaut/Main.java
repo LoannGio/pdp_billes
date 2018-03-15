2d345a1bc597166225eafbc3f66728c08d713080
Loann Giovannangeli <loann.giovannangeli@etu.u-bordeaux.fr>
2018-03-15 16:48:01 +0100

    revert useless modification made to test git push on both repositories

--- pdp_billes/src/_defaut/Main.java
+++ pdp_billes/src/_defaut/Main.java
@@ -7,7 +7,5 @@ public class Main {
 	public static void main(String[] args) {
 		// Initialiser l'instance du controller qui instancie le model
 		MainFrame window = new MainFrame();
-		String testGIT = "useless modification to test git push on both gits repositories"; // TODO
-																							// delete
 	}
 }
