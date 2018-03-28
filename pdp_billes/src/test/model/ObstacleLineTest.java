29ddfe8c9c38222f2dc08355a8f2db385778600c
mmayolini <maxime.mayolini@gmail.com>
2018-03-28 19:23:36 +0200

    added tests

--- pdp_billes/src/test/model/ObstacleLineTest.java
+++ pdp_billes/src/test/model/ObstacleLineTest.java
@@ -33,12 +33,21 @@ public class ObstacleLineTest {
 	public void test_contains() {
 		depart = new Point(50, 50);
 		arrivee = new Point(50, 53);
-		o = new ObstacleLine(depart, arrivee);
+		o = new ObstacleLine(depart, arrivee, 0.5);
 		assertEquals(o.contains(depart), true);
 		depart.setLocation(0, 20);
 		o.setPositions(depart, arrivee);
 		p = new Point(-1, 25);
 		assertEquals(o.contains(p), false);
+	}
+	
+	@Test
+	public void testIsNearPoint(){
+		depart = new Point(50, 50);
+		arrivee = new Point(600, 50);
+		o = new ObstacleLine(depart, arrivee, 0.5);
+		Point p = new Point(300, 52);
+		assertEquals(o.isNearPoint(p), true);
 	}
 
 }
